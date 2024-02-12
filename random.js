b5e1447f141f87dea877947b9cb2b9c9296280dd
William Kowalski <williamkowalski@gmail.com>
2024-02-12 05:07:15 -0400

    Added "Thinking" to wait time and a few more styles

--- random.js
+++ random.js
@@ -1,6 +1,11 @@
 // Show a random piece from a random page
 async function showRandomImage() {
 
+    console.log("Thinking...")
+
+    document.getElementById('thinking').style.display = "block";
+
+
     let pageNum = Math.floor(Math.random() * 10000);
     let imageNum = Math.floor(Math.random() * 11);
     let imageStem = 'https://www.artic.edu/iiif/2/';
@@ -12,12 +17,14 @@ async function showRandomImage() {
     try {
         const response = await fetch(url);
 
+        
+
         if (!response.ok) {
             console.log("Quitting...")
             throw new Error('Network response was bad');
-        }
+        } else {
 
-        const json = await response.json();  // convert json into js
+            const json = await response.json();  // convert json into js
         pageInfo = json.data; // update global pageInfo variable with new data
 
         console.log(pageInfo[imageNum]);
@@ -38,16 +45,14 @@ async function showRandomImage() {
         let country = thisImage.place_of_origin;
         let full_image_link;
 
+        document.getElementById('thinking').style.display = "none";
+
         if(isPublic === true){ // if in public domain, it will have a large zoomable version of the image available
             full_image_link = 'https://www.artic.edu/iiif/2/' + thisImage.image_id + "/full/1686,/0/default.jpg";
             document.getElementById('fullImageLink').innerHTML = `<a href="${full_image_link}" target="_blank"> CLICK HERE FOR FULL SIZE PUBLIC DOMAIN IMAGE</a>`;
         }
 
-        if(description !== null){ // check if there is a description
-            document.getElementById('advDesc').innerHTML = description;
-        } else {
-            document.getElementById('advDesc').innerHTML = "<strong>Item Description:</strong> <br /> No further description available";
-        } 
+
 
     
         document.getElementById('title').innerHTML = "<em>" + title + "</em>"; // name of piece
@@ -68,10 +73,15 @@ async function showRandomImage() {
        
         document.getElementById('description').innerHTML = `<strong>DATE: </strong>${date}<br /><strong>COUNTRY: </strong>${country}<br /><strong>TYPE: </strong>${type}<br /><strong>MEDIUM: </strong>${medium}<br><strong>CREDIT: </strong>${credit}<br /><a href="${image}" target="_blank">CLICK HERE FOR ORIGINAL IMAGE FILE</a>`;
 
-      
-       
-    //    document.getElementById('artistDisplay').innerHTML = artist_display;
-       
+        if(description !== null){ // check if there is a description
+            document.getElementById('advDesc').innerHTML = description;
+        } else {
+            document.getElementById('advDesc').innerHTML = "<strong>Item Description:</strong> <br /> No further description available";
+        }        
+
+        }
+
+        
 
     }
     catch (error) {
